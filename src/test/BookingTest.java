9bf9b86bf147791f8dc2aefb7b0237bd559b0dd7
Elvira Jonsson <elviraj@student.chalmers.se>
2015-12-04 15:23:00 +0100

    Added test package

---
+++ src/test/BookingTest.java
@@ -0,0 +1,19 @@
+
+
+import static org.junit.Assert.*;
+import ClassDiagram.impl.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BookingTest {
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
