781f4f0ad441a82b21bf09ca8aa00241667f36ad
Joel Tegman <gotinkx@hotmail.com>
2015-12-11 14:38:05 +0100

    started test class for RoomBooking

--- src/test/BookingTest.java
+++ src/test/BookingTest.java
@@ -1,7 +1,11 @@
-
+package test;
 
 import static org.junit.Assert.*;
+
+import java.util.Date;
+
 import ClassDiagram.impl.*;
+
 
 import org.junit.Before;
 import org.junit.Test;
@@ -9,11 +13,32 @@ import org.junit.Test;
 public class BookingTest {
 	@Before
 	public void setUp() throws Exception {
+		
+		
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+
+		// Create the instance of RoomBooking
+		ClassDiagramFactoryImpl factory = new ClassDiagramFactoryImpl();
+		RoomBookingImpl roomBook = (RoomBookingImpl) factory.createRoomBooking();
+		
+		// Test Start date
+		 
+		Date startDate = new Date(2015,12,24);
+		roomBook.setStartDate(startDate);
+		int x = roomBook.getStartDate().compareTo(new Date(2015,12,24));
+		 
+		assertTrue(x == 0);
+		
+		// Test End date
+		Date endDate = new Date(2016,01,01);
+		roomBook.setEndDate(endDate);
+		
+		int y = roomBook.getEndDate().compareTo(new Date(2016,01,01));
+		 
+		assertTrue(y == 0);
 	}
 
 }
