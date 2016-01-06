dc2c55a4403823bff4b96b57bca40e457338c535
Elvira Jonsson <elviraj@student.chalmers.se>
2016-01-06 13:56:45 +0100

    Started implementing roomBookingsMock

--- src/MockDatabase/RoomBookingsMock.java
+++ src/MockDatabase/RoomBookingsMock.java
@@ -1,39 +1,66 @@
 package MockDatabase;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
+import ClassDiagram.Customer;
 import ClassDiagram.RoomBooking;
 import ClassDiagram.RoomType;
+import ClassDiagram.impl.ClassDiagramFactoryImpl;
 import DatabaseInterfaces.RoomBookings;
 
 public class RoomBookingsMock implements RoomBookings{
 	
+	private HashMap<Long, RoomBooking> bookings;
+	private static RoomBookingsMock instance = null;
 	
+	private RoomBookingsMock(){
+		bookings = new HashMap<Long, RoomBooking>();
+		RoomTypesMock roomType = RoomTypesMock.getInstance();
+		CustomersMock customers = CustomersMock.getInstance();
+		//TODO: add bookings
+		RoomBooking rb1 = ClassDiagramFactoryImpl.eINSTANCE.createRoomBooking();
+		rb1.addRoomType(roomType.getRoomTypes().get(0));
+		rb1.setStartDate(new Date(2016,1,20));
+		rb1.setEndDate(new Date(2016, 1, 25));
+		rb1.setId(UUID.randomUUID().getLeastSignificantBits());
+		Customer c1 = customers.getCustomer("elvirajonsson123@hotmail.com");
+		c1.addRoomBooking(rb1);
+		
+		
+		
+	}
+	
+	public static RoomBookingsMock getInstance(){
+		if(instance == null){
+			instance = new RoomBookingsMock();
+		}
+		return instance;
+	}
 
 	@Override
 	public boolean addRoomBooking(RoomBooking roomBooking) {
-		// TODO Auto-generated method stub
-		return false;
+		RoomBooking result = bookings.put(roomBooking.getId(), roomBooking);
+		return result == null || result.equals(roomBooking);
 	}
 
 	@Override
 	public boolean deleteRoomBooking(UUID id) {
-		// TODO Auto-generated method stub
-		return false;
+		RoomBooking result = bookings.remove(id.getLeastSignificantBits());
+		return result != null;
 	}
 
 	@Override
 	public boolean updateRoomBooking(RoomBooking roomBooking) {
-		// TODO Auto-generated method stub
-		return false;
+		RoomBooking result = bookings.put(roomBooking.getId(), roomBooking);
+		return result.equals(roomBooking);
 	}
 
 	@Override
 	public RoomBooking getRoomBooking(UUID id) {
-		// TODO Auto-generated method stub
-		return null;
+		return bookings.get(id.getLeastSignificantBits());
 	}
 
 	@Override
